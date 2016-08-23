3bd8811713a2edbf9e3051c51b54f251003ae633
wangbc@infogreat.cn <wangbc@infogreat.cn>
2016-08-23 17:21:57 +0800

    wangbc

--- src/app/common/constants/app-configs.js
+++ src/app/common/constants/app-configs.js
@@ -1,7 +1,8 @@
 'use strict';
 var AppConfigs = {
 	API_BASE_URL: '${API.BASE}/api/',
-	USER_TOKEN_KEY: 'x-access-token'
+	USER_TOKEN_KEY: 'x-access-token',
+	UPLOAD_DOMAIN: 'http://oc3g3d3gr.bkt.clouddn.com/',
 };
 
 module.exports = {
--- src/app/common/controllers/base-crud-ctrl.js
+++ src/app/common/controllers/base-crud-ctrl.js
@@ -10,7 +10,8 @@ const _ = require('lodash');
  * @param {[type]} ApiSrv       [description]
  * @param {[type]} vm           控制器的value model
  * @param {[type]} ctrlOpts     {
- *                                  modelName,  // 必须 String, 模型的名称 *                                   
+ *                                  modelName,  // 必须 String, 模型的名称 * 
+ *                                  beforeSave, // 非必须 function 保存前处理                                  
  *                              }
  */
 function BaseCrudCtrl($window, $state, $stateParams, ApiSrv, vm, ctrlOpts) {
@@ -28,6 +29,7 @@ function BaseCrudCtrl($window, $state, $stateParams, ApiSrv, vm, ctrlOpts) {
 
     function _saveForNew() {
         let listState = 'main.' + ctrlOpts.modelName + '.list';
+
         ApiSrv.exec(ctrlOpts.modelName + '/create', vm.model)
             .then(function() {
                 $state.go(listState);
@@ -56,6 +58,9 @@ function BaseCrudCtrl($window, $state, $stateParams, ApiSrv, vm, ctrlOpts) {
     };
 
     vm.save = function() {
+        if (ctrlOpts.beforeSave) {
+            ctrlOpts.beforeSave();
+        }                
         if (vm.action === 'new') {
             _saveForNew();
         } else if (vm.action === 'edit') {
