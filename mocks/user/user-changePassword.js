02caa50255e18901fa8deaa6a936c44c62034a40
wangbc@infogreat.cn <wangbc@infogreat.cn>
2016-09-12 14:46:37 +0800

    changePasswor

--- mocks/user/user-changePassword.js
+++ mocks/user/user-changePassword.js
@@ -1,6 +1,7 @@
 /**
  * mock for 用户修改密码
  * @type {Object}
+ * params: {oldPassword: "123456", newPassword: "222222"}
  */
 module.exports = {
     '/api/user/changePassword': {
--- mocks/user/user-list.js
+++ mocks/user/user-list.js
@@ -20,27 +20,32 @@ module.exports = {
                         id: 1,
                         name: 'test1',
                         mail: 'test1@test.com',
-                        deleteFlag: '0'
+                        deleteFlag: '0',
+                        updatedAt:"2016-09-08T07:33:12.000Z"
                     }, {
                         id: 2,
                         name: 'test2',
                         mail: 'test2@test.com',
-                        deleteFlag: '1'
+                        deleteFlag: '1',
+                        updatedAt:"2016-09-08T07:33:12.000Z"
                     }, {
                         id: 3,
                         name: 'test3',
                         mail: 'test3@test.com',
-                        deleteFlag: '0'
+                        deleteFlag: '0',
+                        updatedAt:"2016-09-08T07:33:12.000Z"
                     }, {
                         id: 4,
                         name: 'test4',
                         mail: 'test4@test.com',
-                        deleteFlag: '0'
+                        deleteFlag: '0',
+                        updatedAt:"2016-09-08T07:33:12.000Z"
                     }, {
                         id: 5,
                         name: 'test5',
                         mail: 'test5@test.com',
-                        deleteFlag: '0'
+                        deleteFlag: '0',
+                        updatedAt:"2016-09-08T07:33:12.000Z"
                     }, ]
                 }
             }
